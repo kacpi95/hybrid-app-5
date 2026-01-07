3537b27f4f8ab01cf5674a95e5a2a74181f7cf45
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-07 11:44:50 +0100

    install package

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>You clicked {count} times</Text>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </View>
   );
 }
