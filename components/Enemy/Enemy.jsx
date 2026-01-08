75173411e9bf2d47854e6bb2fb0854e585bd3425
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-08 12:55:40 +0100

    add: Enemy component with UI enemy

---
+++ components/Enemy/Enemy.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Image, View, Text } from 'react-native';
+
+export default function Enemy({ name, strenght, image }) {
+  const [stamina, setStamina] = useState(50);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{name}</Text>
+      {image && <Image src={image} style={styles.image} />}
+      <Text style={styles.strenght}>Siła: {strenght}</Text>
+      <Text style={styles.stamina}>Wytrzymałość: {stamina}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
