e37715ff1d0ea71c6ae1a844bab1fdc30ab84140
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-08 12:57:36 +0100

    clear App

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <View style={styles.container}>
-      <Text>You clicked {count} times</Text>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </View>
-  );
+  return;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
